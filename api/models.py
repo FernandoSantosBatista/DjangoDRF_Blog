738b56be9a9d0bb410f904ce39f22ab7a6cd503b
FernandodotDev <fernandodossantos005@gmail.com>
2022-08-10 17:16:06 -0300

    first commit

---
+++ api/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField()
+    description = models.TextField()
+    body = models.TextField()
+    image = models.ImageField(upload_to="uploads/", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.slug
+
+    def get_image(self):
+        if self.image:
+            return 'http://127.0.0.1:8000' + self.image.url
+        return ''
+
+
